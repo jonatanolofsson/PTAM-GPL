f84b5d79d5e3a23d402585f6adc108434295fc40
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2014-12-23 13:19:14 +0100

    Improve deps building

--- deps/make_cvd.sh
+++ deps/make_cvd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/deps
+cd $DIR
 (
     cd cvd
     export CXXFLAGS=-D_REENTRANT
     ./configure --without-ffmpeg CPPFLAGS=-I$DIR/deps
     make
-    git clean -fdx -e libcvd.a
+    git clean -fdx -e libcvd.a -e cvd/config.h
 )
--- deps/make_gvars.sh
+++ deps/make_gvars.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/deps
+cd $DIR
 (
     cd gvars
     ./configure --disable-widgets CPPFLAGS=-I$DIR/deps
     make
-    git clean -fdx -e libGVars.a
+    git clean -fdx -e libGVars3.a -e gvars3/config.h
 )
 
