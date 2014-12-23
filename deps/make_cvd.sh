2537d254a4ab6a199d231aa81c97e8b5335db544
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2014-12-23 12:20:42 +0100

    Add deps building to tup build chain

---
+++ deps/make_cvd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/deps
+(
+    cd cvd
+    export CXXFLAGS=-D_REENTRANT
+    ./configure --without-ffmpeg CPPFLAGS=-I$DIR/deps
+    make
+    git clean -fdx -e libcvd.a
+)
--- deps/make_gvars.sh
+++ deps/make_gvars.sh
@@ -2,14 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/deps
 (
-    cd cvd
-    export CXXFLAGS=-D_REENTRANT
-    ./configure --without-ffmpeg CPPFLAGS=-I$DIR/deps
-    make
-)
-(
     cd gvars
     ./configure --disable-widgets CPPFLAGS=-I$DIR/deps
     make
+    git clean -fdx -e libGVars.a
 )
 
